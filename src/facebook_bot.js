e479644440226b929e4971a24d73495d972bd91f
Nguyen Thanh Hai <haint@co-well.jp>
2017-10-14 00:21:09 +0900

    init

--- src/facebook_bot.js
+++ src/facebook_bot.js
@@ -1,6 +1,60 @@
+var logger = require('morgan');
+var http = require('http');
+var bodyParser = require('body-parser');
+var express = require('express');
+var request = require('request');
+var router = express();
+
+
 const uuid = require('uuid');
-const request = require('request');
 const async = require('async');
+
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+var server = http.createServer(app);
+
+app.listen(process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+  res.send("Server chạy ngon lành.");
+});
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.verify_token'] === 'haint') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
+
+// Đoạn code xử lý khi có người nhắn tin cho bot
+app.post('/webhook', function(req, res) {
+  var entries = req.body.entry;
+  for (var entry of entries) {
+    var messaging = entry.messaging;
+    for (var message of messaging) {
+      var senderId = message.sender.id;
+      if (message.message) {
+        // Nếu người dùng gửi tin nhắn đến
+        if (message.message.text) {
+          var text = message.message.text;
+          if(text == 'hi' || text == "hello")
+          {
+            sendMessage(senderId, "Hello em iu");
+          }
+          else{sendMessage(senderId, "Em iu đang làm gì đó" + senderId);}
+        }
+      }
+    }
+  }
+
+  res.status(200).send("OK");
+});
+
+
 
 const FB_PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN || 'EAAXDLESkwZCkBAEUsQFMVp2rM3o3ERLPCUHnbUYLlT9yCt8nvKcAhM0q1U6iMdXTQTalVtroJUXkObDVJ6emx590YLhuvQD0OMc9LL2JWAEIoBbBtaYF1ZCOtGInqpRbkZB3CNdKeXtpegZARp5bRlH3Cjq3mTyDOrODVC8mVgZDZD';
 const FB_TEXT_LIMIT = 640;
